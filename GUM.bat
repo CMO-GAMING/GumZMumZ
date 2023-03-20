b1247fc679eaf29cf9a83b516b93d72c6d83f4e4
DeGrinch Xtocsea <DeGrinch@users.noreply.github.com>
2023-03-20 19:38:31 -0400

    Update GUM.bat

--- GUM.bat
+++ GUM.bat
@@ -1,103 +1,80 @@
 @echo off
 :START
-TITLE DayZServer CORE FILE - GLOBAL UPDATE MANAGER (GUM)
+TITLE MOD UPDATE MANAGER
 COLOR 0A
-:: Configure the below "sets" to suit your system or directory setup.
-
-set SCRIPTTITTLE="G.U.M"
+:: Configurable Variables Are listed below ::
+set SCRIPTTITLE=M.U.M
 :: Name of THIS BATCH FILE :: 
 :: Helps with debug & window reference, when needed.  
-set TITLE="GUM.bat" 
+set TITLE="MUM" 
+set GUM="MUM.bat"
+:: SERVER ROOT DIRECTORY ::
 
-:: DAYZGHOSTFILES AKA DAYZSERVER REPOSITORY FILES ::
-:: This is the directory where the original dayzserver files will be downloaded and stored ::
-:: These files will be updated by steamcmd when the GUM.bat script is executed and reboots every xx minutes ::
-:: GUM.bat SCRIPT updates the DAYZSERVERFILES itself, it connects to the steamcmd and updates all dayserver files to their current release ::
-set DAYZGHOSTFILES="PATHTOYOURDAYZSERVERREPO"
-
-:: GAME SERVER ROOT DIRECTORY ::
-:: This would be the directory where the default DayZServer_x64.exe file is located 
-:: By default dayzserver is installed at C:\Steamlibrary\steamapps\common\DayZServer\
-:: You can modify this  if your directory is different. This directory will be where your instance(s) of modified dayzservers will reside.
-set DAYZSERVER_DIRECTORY="PATHTOYOURDAYZGAMESERVER"
-
+:: MOD UPDATE LIST ::
+:: This points to your modlist.txt file. 
+:: You will have had to created this txt file manually before starting the server.
+:: This is a required file if you want to auto udate your server. 
+:: The Modlist.txt must be unique per the dayz server instance it belongs to IE Modlist1.txt Modlist2.txt etc
+:: This is assuming you want the servers to start faster by updating only the individual mods assigned in .bat file.
+:: The contents of the file must be accurate "MODIDNUMBER,@MODNAME" one mod per line
+:: You can ignore this, or leave it blank if you perefer manually updating your server(s)
+:: If you are still not sure how this works... Join our Discord https://discord.gg/KK6KAvvD for more information . 
+set MOD_LIST=(G:\MyDayZGameServer\GumZMumZ\MOD_LIST_GLOBAL.txt)
 :: STEAMCMD DIRECTORY AND USER INFORMATION ::
 :: Below you will need to modify/set the location of your steamcmd Directory location of steam workshop, steam user name. 
-set STEAMCMD_LOCATION=C:PATHTOYOUR\steamCMD
-:: This is where you put your steam workshop directory details
-set STEAM_WORKSHOP=C:PATHTOYOUR\steamCMD\steamapps\workshop\content\221100
-
+set STEAMCMD_LOCATION=G:\SteamCMD1
+:: This is where you put the path to your steam workshop directory
+set STEAM_WORKSHOP=G:\SteamCMD1\steamapps\workshop\content\221100
 :: Enter your steam user name. Do not worry about a password.
 set STEAM_USER=YOURSTEAMUSERNAME
 :: No need to edit this next line...
-set STEAMCMD_DEL=10
+set STEAMCMD_DEL=5
 
-:: TIMEOUT FOR SCRIPT RESTART ::
-set TIMEOUT1=1200
+:: THINGS TO KNOW REGARDING STEAM LOGIN ::
+:: SteamCMD Will ask for your password at the time of first boot from a new device. Password is then kept in a secure hash. 
+:: You will only be prompted to enter password when you have deleted that hash. 
+:: HOWEVER, if you have 2FA enabled ** such as email authorization code ** this will continue to be a problem every time the server auto-reboots.
+:: 2FA interferes with the seamless login, update, mod update, server boot up process.
 
-:UPDDayZServer
-@timeout 1 > NUL
-echo Welcome...
-@timeout 1 > NUL 
-echo                  Now initiating %SCRIPTTITTLE%.
-@timeout 1 > NUL
-cls
-echo --PLEASE NOTE-- This script will ONLY update your CORE server files.
-@timeout 1 > NUL
-cls
-echo --PLEASE NOTE-- This Script DOES NOT update your DAYZ MOD files.
-@timeout 1 > NUL
-cls
-echo --PLEASE NOTE-- To update your DAYZ MODS make sure you are running file: MUM.Bat
-@timeout 15 > NUL
-cls
-goto STEAMCMD
+:: How often do you want MUM to check SteamCMD for updates
+:: The default setting is 1800 seconds (30 minutes)
+set TIMEOUT1=1800
 
-:STEAMCMD
-@echo off
+
+:: THERE IS ABSOLUTELY NO NEED TO MODIFY THE CONTENT BELOW THIS LINE ::
+
+::::::::::::::
+goto CHECKMODS
+:CHECKMODS
+cls
+FOR /L %%s IN (2,-1,0) DO (
+	cls
+	echo Checking for mod updates in %%s seconds.. 
+	timeout 1 >nul
+)
+echo Reading in configurations/variables set in this batch and MOD_LIST. Updating Steam Workbench mods...
+@ timeout 1 >nul
 cd %STEAMCMD_LOCATION%
-steamcmd.exe +force_install_dir "%DAYZGHOSTFILES%" +login "%STEAM_USER%" +app_update 223350 validate +quit
-@timeout 1
+for /f "tokens=1,2 delims=," %%g in %MOD_LIST% do steamcmd.exe +login %STEAM_USER% +workshop_download_item 221100 "%%g" +quit +cls
 cls
-echo ------------ DayZServer Files updated, and/or validating process has completed
-@timeout 1 > NUL
-cls
-goto TRANSFER
+echo Steam Workshop files/mods are up to date.
+@ timeout 5 >nul
+goto WAIT
 
-:INFO
-echo Seeing a report of "0 Files were copied" is normal, it means there are NO updated files to copy
-@timeout 1 > NUL
-echo Seeing a report of "can't read file:..." should be expected FOR THESE FILES: 
-@timeout 1 > NUL
-echo whitelist.txt - Marked as exempt
-@timeout 1 > NUL
-echo serverDZ.cfg - Marked as exempt
-@timeout 1 > NUL
-echo ------  Files that have been marked as exempt are config files.
-echo ------  Config files are any files that are manually edited to suit your server setup.
-echo ------  Overwriting these files in your game server directory WILL BREAK your server.
-@timeout 5 > NUL
+:WAIT
 cls
-goto TRANSFER
-
-:TRANSFER
-echo ------ New or updated core server files now being transfered to: 
-echo ------ %DAYZSERVER_DIRECTORY%
-xcopy %DAYZGHOSTFILES% %DAYZSERVER_DIRECTORY% /Z /U /Y /R
-echo Seeing a report of "0 Files were copied" above, is normal, it means there are NO updated files to copy
-@timeout 2 > NUL
-echo Seeing a report of "can't read file:..." above, should be expected FOR THESE FILES: 
-@timeout 15 > NUL
-cls
-goto restart 
-
-:RESTART 
-echo This system is monitoring Steam Dayz Server App (code: 223350)
-echo The GUM process will repeat once every 5 minutes...
-timeout %TIMEOUT1%
+echo Please keep this window open to continue running Mod Update Manager.
+echo Closing this window will terminate MUM.
+echo MUM will check for upates every 15 Minutes.
+echo To RESTART this process early press CTRL+C enter N at the prompt.
+echo To TERMINATE this window press CTRL+C enter Y at the prompt.
+echo Otherwise let this process do what it does.
+echo.
+echo If you need support or to see whats next in GumZMumz 1.2 "The Jester", Join our Discord https://discord.gg/KK6KAvvD community
+timeout %TIMEOUT1% /nobreak >nul
+echo Restarting Mod Update Manager
+taskkill /%DZSA_EXE_RENAME% /F
 ::Time in seconds to wait before..
-timeout 5
+timeout 2
 ::Go back to the top and repeat the whole cycle again
-cls
-goto START 
-
+goto START
