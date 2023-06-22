9d8097bc21df17442e4daea92e9aef2594719fc0
De Grinch <DeGrinch@users.noreply.github.com>
2023-06-22 08:03:49 -0400

    Update GUM.bat

--- GUM.bat
+++ GUM.bat
@@ -1,34 +1,37 @@
 @echo off
+color 0A
+title GUM-GLOBAL UPDATE MANAGER
+set SCRIPTTITTLE="G.U.M - GLOBAL UPDATE MANAGER"
+set SCRIPTVERSION="GumZMumz 1.0" 
 :START
-TITLE DayZServer CORE FILE - GLOBAL UPDATE MANAGER (GUM)
-COLOR 0A
-:: Configure the below "sets" to suit your system or directory setup.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Please change the variables below to match your current DayZServer System Configuration               ::
+:: When you run this script a cmd prompt will open up and connect to SteamCMD                            ::
+:: You will be prompted for the password that goes with the steam username you declare in variable below ::
+:: Wait for the download or verification process to indicate your core files are up to date              ::
+:: Leave GUM window running to check for updates every 15 minutes                                        ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-set SCRIPTTITTLE="G.U.M"
-:: Name of THIS BATCH FILE :: 
-:: Helps with debug & window reference, when needed.  
-set TITLE="GUM.bat" 
-
-:: DAYZGHOSTFILES AKA DAYZSERVER REPOSITORY FILES ::
-:: This is the directory where the original dayzserver files will be downloaded and stored ::
-:: These files will be updated by steamcmd when the GUM.bat script is executed and reboots every xx minutes ::
-:: GUM.bat SCRIPT updates the DAYZSERVERFILES itself, it connects to the steamcmd and updates all dayserver files to their current release ::
-set DAYZGHOSTFILES="PATHTOYOURDAYZSERVERREPO"
-
-:: GAME SERVER ROOT DIRECTORY ::
-:: This would be the directory where the default DayZServer_x64.exe file is located 
-:: By default dayzserver is installed at C:\Steamlibrary\steamapps\common\DayZServer\
-:: You can modify this  if your directory is different. This directory will be where your instance(s) of modified dayzservers will reside.
-set DAYZSERVER_DIRECTORY="PATHTOYOURDAYZGAMESERVER"
+:: GHOSTFILES ::
+:: Directory where the CORE dayzserver files have been *or will be* downloaded by the script, and archived::
+:: NOTE: You will want to create this directory, before running this script, if DIR is not already present::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set DAYZGHOSTFILES="G:\dayzserver"
 
 :: STEAMCMD DIRECTORY AND USER INFORMATION ::
-:: Below you will need to modify/set the location of your steamcmd Directory location of steam workshop, steam user name. 
-set STEAMCMD_LOCATION=C:PATHTOYOUR\steamCMD
-:: This is where you put your steam workshop directory details
-set STEAM_WORKSHOP=C:PATHTOYOUR\steamCMD\steamapps\workshop\content\221100
+:: Below you will need to modify/set the location of your SteamCMD Directory. If you are following our    ::
+:: Instructions for this installation then this line is already accurate, if you are configuring GumZMumz ::
+:: to run with a previous installation of steamcmd and dayzserver you may need to change this variable.   ::
+:: The directory where your steamcmd.exe can be found                                                     ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set STEAMCMD_LOCATION=G:\steamcmd 
 
-:: Enter your steam user name. Do not worry about a password.
-set STEAM_USER=YOURSTEAMUSERNAME
+:: YOUR STEAM USERNAME ::
+:: Enter your steam user name. Do not worry about a password. If this is your first time running GUM.bat  ::
+:: you will be prompted for a password when you run the script. If you have previously run steamcmd.exe   ::
+:: the steamcmd directory will already have your user criteria stored. GUMZMUMZ                           ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set STEAM_USER=CHANGETHISTOYOURUSERNAME
 :: No need to edit this next line...
 set STEAMCMD_DEL=10
 
@@ -36,21 +39,21 @@ set STEAMCMD_DEL=10
 set TIMEOUT1=1200
 
 :UPDDayZServer
-@timeout 1 > NUL
-echo Welcome...
-@timeout 1 > NUL 
-echo                  Now initiating %SCRIPTTITTLE%.
+echo Welcome... %SCRIPTTITTLE% %SCRIPT_VERSION%
 @timeout 1 > NUL
 cls
-echo --PLEASE NOTE-- This script will ONLY update your CORE server files.
-@timeout 1 > NUL
+echo --PLEASE NOTE-- 
+echo 				This script will ONLY update Server CORE server files.
+@timeout 2 > NUL
+echo --PLEASE NOTE-- 
+echo 				This Script DOES NOT update Server MOD files.
+@timeout 2 > NUL
+echo --PLEASE NOTE-- 
+echo				To update Server MODS make sure you are running: MUM.Bat
+@timeout 8 > NUL
 cls
-echo --PLEASE NOTE-- This Script DOES NOT update your DAYZ MOD files.
-@timeout 1 > NUL
-cls
-echo --PLEASE NOTE-- To update your DAYZ MODS make sure you are running file: MUM.Bat
-@timeout 15 > NUL
-cls
+@timeout 2 > NUL 
+echo                 Initiating %SCRIPTTITTLE%.
 goto STEAMCMD
 
 :STEAMCMD
@@ -62,42 +65,26 @@ cls
 echo ------------ DayZServer Files updated, and/or validating process has completed
 @timeout 1 > NUL
 cls
-goto TRANSFER
+goto WAIT
 
-:INFO
-echo Seeing a report of "0 Files were copied" is normal, it means there are NO updated files to copy
-@timeout 1 > NUL
-echo Seeing a report of "can't read file:..." should be expected FOR THESE FILES: 
-@timeout 1 > NUL
-echo whitelist.txt - Marked as exempt
-@timeout 1 > NUL
-echo serverDZ.cfg - Marked as exempt
-@timeout 1 > NUL
-echo ------  Files that have been marked as exempt are config files.
-echo ------  Config files are any files that are manually edited to suit your server setup.
-echo ------  Overwriting these files in your game server directory WILL BREAK your server.
-@timeout 5 > NUL
+:WAIT
 cls
-goto TRANSFER
-
-:TRANSFER
-echo ------ New or updated core server files now being transfered to: 
-echo ------ %DAYZSERVER_DIRECTORY%
-xcopy %DAYZGHOSTFILES% %DAYZSERVER_DIRECTORY% /Z /U /Y /R
-echo Seeing a report of "0 Files were copied" above, is normal, it means there are NO updated files to copy
-@timeout 2 > NUL
-echo Seeing a report of "can't read file:..." above, should be expected FOR THESE FILES: 
-@timeout 15 > NUL
-cls
-goto restart 
-
-:RESTART 
-echo This system is monitoring Steam Dayz Server App (code: 223350)
-echo The GUM process will repeat once every 5 minutes...
-timeout %TIMEOUT1%
-::Time in seconds to wait before..
-timeout 5
-::Go back to the top and repeat the whole cycle again
+echo Process has completed. Core DayZServer files are now up to date
+echo Keep this window open to continue running Global Update Manager.
+echo Closing this window will terminate GUM.
+echo GUM will check for upates every 15 Minutes by default.
+echo To RESTART this process early press CTRL+C enter N at the prompt.
+echo To TERMINATE this window press CTRL+C enter Y at the prompt.
+echo Otherwise allow this process/script to do what it does.
+echo.
+echo If you need support, to be a part of our community, or to enquire 
+echo             .....  what's next for GumZMumz  ... 
+echo 				Join our Discord 
+echo.
+echo                          https://discord.gg/KK6KAvvD community
+timeout %TIMEOUT1% /nobreak >nul
+timeout 1
 cls
 goto START 
 
+:: More updates to come in the near future ::
